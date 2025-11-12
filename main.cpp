37bfb78145d9f24d880a800428ccaef7bbf865fc
PatriciaSaeed <githubuni1300@gmail.com>
2025-11-12 15:56:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,46 @@
 #include <iostream>
 using namespace std;
 
+animal::animal();
+    animal(string n , int a , bool i ){
+    
+    name = name ;
+    age = age ;
+    isHungry = i ;
+    
+    
+    
+    
+    }
+  animal::  ~animal();
+   animal:: void display(){
+   cout << name <<'(' << ""<< age << ',' << getisHungry() << endl ; 
+   
+   
+   
+   };
+   animal:: void feed();
+   animal:: void setname ();
+   animal:: string getname();
+   animal:: void setage ();
+    animal::int getage();
+    animal::void setishungry ();
+    animal::bool getisHungry();
+    
+    
+    
+    
+    
+    
+    
+    
+      visitor :: void displayinfo(){
+      
+      cout << "visitor info :" <<endl ;
+      cout << "name : "<<  getvisitorname();
+      
+      
+      }
 
 // ============== MAIN FUNCTION ==============
 int main() {
